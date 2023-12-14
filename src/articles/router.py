75914b792c9bd1b954f29cc74fc8c3d635dab0e1
Ivan Baklanov <baklanovivan25@gmail.com>
2023-12-14 22:30:50 +0500

    Added pagination for comments article

--- src/articles/router.py
+++ src/articles/router.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 import httpx
 from comments.models import CommentModel
 from src.articles.models import ArticleCollection, ArticleModel
-from fastapi import APIRouter, Depends, HTTPException, Request, Form
+from fastapi import APIRouter, Depends, HTTPException, Query, Request, Form
 from fastapi.templating import Jinja2Templates
 from src.database import blog_collection
 from src.common.redis_utils import get_redis
@@ -23,17 +23,60 @@ async def list_articles(request: Request):
 article_cache = TTLCache(maxsize=256, ttl=30)
 
 @router.get("/{article_id}", response_model=ArticleModel, response_description="Show an article by ID")
-async def show_article(article_id: str, request: Request, redis: redis.Redis = Depends(get_redis)):
+async def show_article(article_id: str, request: Request, page: int = Query(1, alias="page"), redis: redis.Redis = Depends(get_redis)):
+    # Пытаемся получить кэшированную страницу
     cached_article = article_cache.get(article_id)
 
+    # Если страница есть в кэше, то выводим ее
     if cached_article:
-        return templates.TemplateResponse(name="article.html", context={"request": request, "article": cached_article})
+        
+        comments = cached_article["comments"]
+        comments_per_page = 10  # Кол-во комментариев на странице
+        total_comments = len(comments)
+        total_pages = (total_comments + comments_per_page - 1) // comments_per_page
+
+        start_idx = (page - 1) * comments_per_page
+        end_idx = start_idx + comments_per_page
+        paginated_comments = comments[start_idx:end_idx]
+
+        return templates.TemplateResponse(
+            name="article.html", 
+            context={
+                    "request": request, 
+                    "article": cached_article,
+                    "paginated_comments": paginated_comments,
+                    "total_pages": total_pages,
+                    "current_page": page,
+                },
+            )
 
     article = await blog_collection.find_one({"_id": ObjectId(article_id)})
 
+    # Если страница не в кэше, добавляем ее в кэш
     if article:
+
+        # Добавялем пагинацию к комментариям
+        comments = article["comments"]
+        comments_per_page = 10 # Кол-во комментариев на странице
+        total_comments = len(comments)
+        total_pages = (total_comments + comments_per_page - 1) // comments_per_page
+
+        # Разделение комментариев на страницы
+        start_idx = (page - 1) * comments_per_page
+        end_idx = start_idx + comments_per_page
+        paginated_comments = comments[start_idx:end_idx]
+
         article_cache[article_id] = article
-        return templates.TemplateResponse(name="article.html", context={"request": request, "article": article})
+        return templates.TemplateResponse(
+            name="article.html", 
+            context={
+                    "request": request, 
+                    "article": article,
+                    "paginated_comments": paginated_comments,
+                    "total_pages": total_pages,
+                    "current_page": page,
+                },
+            )
 
     raise HTTPException(status_code=404, detail="Article not found")
 
